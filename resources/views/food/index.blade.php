4eb206efaa8ca7a970ca84490e5028b214df6a42
BrunoMartins <BrunoMartins11>
2018-02-10 22:49:42 +0000

    added containor

--- resources/views/food/index.blade.php
+++ resources/views/food/index.blade.php
@@ -2,30 +2,58 @@
 
 @section('content')
 
-<form action="/food/create" method="get">
-
-    <div class="form-group row mb-0">
+<form action="/food/create" method="get" >
+ <div style="text-align: right;">
+    <div class="form-group row mb-0" >
         <div class="col-md-2 offset-md-2">
             <button type="submit" class="btn btn-primary">
                 Add new meal
             </button>
         </div>
     </div>
-
+</div>
 </form>
 </br>
-            <ul style="color:black;">
-                <li>
-                    <p>Meal mean:</p>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Weakly Consumption Mean</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+        
+                
+                    
                     <p>Mean Protein: {{$mean_prot}}</p>
                     <p>Mean Fat: {{$mean_fat}}</p>
                     <p>Mean Carbohydrates: {{$mean_carbo}}</p>
-                </li>
-            </ul>
-
+                
+            
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
                 </br>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Consumption Report</div>
 
-        <p>Last week consumption:</p>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+        
         <ul style="color:black;">
         @foreach ($foods as $food)
                 <li>
@@ -36,7 +64,11 @@
                 </li>
         @endforeach
         </ul>
-
-
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+                
 @endsection
         
