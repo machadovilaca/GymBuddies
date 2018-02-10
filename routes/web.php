e8431211e96307abb1c9c4f4c0ba5d2dc1c7f446
Oliver Galt <gonca2372@gmail.com>
2018-02-10 16:07:19 +0000

    slider -> and routing

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/food', function () {
+    return view('food');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
