246902ff05ffe46f19ee01e8867b31eeb448a77c
machadovilaca <machadovilaca@protonmail.ch>
2018-02-10 14:27:43 +0000

    feature - auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'age' => $data['age'],
+            'height' => $data['height'],
+            'weight' => $data['weight'],
         ]);
     }
 }
